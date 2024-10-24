c94ad67dac37051a942bc9bc0817a2e1914e0a72
summerglaze <paulinafischerolsz@gmail.com>
2024-10-24 11:04:25 +0200

    Dodaj pliki projektów.

---
+++ wpf-cw2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_cw2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            txtFunction.Text = String.Empty;
+            txtResult.Text = String.Empty;
+            double doubleWspA;
+            double doubleWspB;
+            double doubleWspC;
+            if (double.TryParse(wspA.Text, out doubleWspA) && double.TryParse(wspB.Text, out doubleWspB) && double.TryParse(wspC.Text, out doubleWspC)) 
+            {
+                QuadraticFunction qf = new QuadraticFunction(doubleWspA, doubleWspB, doubleWspC);
+                txtResult.Text = qf.calculate().ToString();
+                txtFunction.Text = qf.showFunction();
+            } else
+            {
+                txtResult.Text = "Nieprawidłowe parametry";
+            }
+        }
+    }
+}
---
+++ wpf-cw2/QuadraticFunction.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_cw2
+{
+    class QuadraticFunction
+    {
+        public double WspA { get; } //set jest private
+        public double WspB { get; }
+        public double WspC { get; }
+
+        public QuadraticFunction(double wspA, double wspB, double wspC)
+        {
+            WspA = wspA; 
+            WspB = wspB; 
+            WspC = wspC;
+        }
+
+        public string calculate()
+        {
+            
+            if (WspA == 0)
+            {
+                return "a=0"+ System.Environment.NewLine + "To nie jest równanie kwadratowe.";
+            } else
+            {
+                double result;
+                double delta = WspB * WspB - 4 * WspA * WspC;
+
+                if (delta < 0)
+                {
+                    return "Delta < 0." + System.Environment.NewLine + "Brak rozwiązań.";
+
+                } else if (delta == 0)
+                {
+                    result = -WspB / (2*WspA);
+                    return "Rozwiązanie x="+result;
+                } else
+                {
+                    double x1 = (-WspB + Math.Sqrt(delta)) / (2 * WspA);
+                    double x2 = (-WspB - Math.Sqrt(delta)) / (2 * WspA);
+                    return "Rozwiązaniem są dwa miejsca zerowe x1=" + x1 + ", x2=" + x2;
+                }
+            }
+        }
+
+        public string showFunction()
+        {
+            string inputFunction = "";
+            if (WspA < 0) { inputFunction += "(" + WspA + ")*x2"; } else if (WspA>0) { inputFunction = WspA.ToString()+ "*x2"; }
+            if (WspA !=0 && WspB!=0) { inputFunction += " + "; }
+            if (WspB < 0) { inputFunction += "(" + WspB + ")*x"; } else if (WspB>0) { inputFunction += WspB+ "*x"; }
+            if ((WspB != 0 && WspC != 0) || (WspB==0 && WspA !=0 && WspC != 0)) { inputFunction += " + "; }
+            if (WspC < 0) { inputFunction += "(" + WspC + ")"; }else if (WspC>0){inputFunction += WspC;}
+                return inputFunction;
+        }
+    }
+}
